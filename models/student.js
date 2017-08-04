9c5477b8d7dfd8b0545390fdc353acdd868265cb
Cameron Chaparro <cccons@tuta.io>
2017-08-03 22:22:51 -0500

    Update formatting of `student` / `teacher` models
    Give a more consistent formatting for the `student` and `teacher` models
    that matches the other models.

--- models/student.js
+++ models/student.js
@@ -2,12 +2,12 @@ const mongoose   = require('mongoose')
     , mongooseuv = require('mongoose-unique-validator');
 
 const schema = new mongoose.Schema({
-  'firstname': { type: String, required: true },
-  'lastname' : { type: String, required: true },
-  'email'    : { type: String, required: true, unique: true },
-  'username' : { type: String, required: true, unique: true },
-  'password' : { type: String, required: true },
-  'results'  : [{ type: mongoose.Schema.ObjectId, ref: 'ExamResult' }]
+  'firstname' : { type: String, required: true },
+  'lastname'  : { type: String, required: true },
+  'email'     : { type: String, required: true, unique: true },
+  'username'  : { type: String, required: true, unique: true },
+  'password'  : { type: String, required: true },
+  'results'   : [{ type: mongoose.Schema.ObjectId, ref: 'ExamResult' }]
 });
 
 schema.plugin(mongooseuv);
