c25a12931fd9e7afed986d2814a73e5e54c3dfb7
Cameron Chaparro <cccons@tuta.io>
2017-08-03 22:12:06 -0500

    Update the `student` model
    Remove the `exams` field and add an `email` field.
    
    For more details, refer to:
    https://github.com/cccons/elle/issues/5#issuecomment-320144699

--- models/student.js
+++ models/student.js
@@ -4,9 +4,9 @@ const mongoose   = require('mongoose')
 const schema = new mongoose.Schema({
   'firstname': { type: String, required: true },
   'lastname' : { type: String, required: true },
+  'email'    : { type: String, required: true, unique: true },
   'username' : { type: String, required: true, unique: true },
   'password' : { type: String, required: true },
-  'exams'    : [{ type: mongoose.Schema.ObjectId, ref: 'Exam' }],
   'exam_res' : [{ type: mongoose.Schema.ObjectId, ref: 'ExamResult' }]
 });
 
